9c8157ef79efecee669f79c44a73e2562f40be13
Rafael <rafaelrgsenhorinho@gmail.com>
2024-02-26 16:38:27 -0300

    aprofundamento do projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,96 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Aluno(
+    val nome: String,
+    val email: String,
+    val dataNascimento: String
+)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
+data class ConteudoEducacional(    
+    var nome: String,
+    var duracao: Int = 60,
+    var descricao: String,
+    var nivelDificuldade: Nivel,
+    var categoria: String)	{
+    
+    fun atualizarConteudo(novoNome: String, novaDuracao: Int, novaDescricao: String, novaDificuldade: Nivel, novaCategoria: String) {
+        nome = novoNome
+        duracao = novaDuracao
+        descricao = novaDescricao
+        nivelDificuldade = novaDificuldade
+        categoria = novaCategoria
+    }
+}
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableListOf<Aluno>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val formacoes = mutableListOf<Formacao>()
+    
+    fun matricular(aluno: Aluno) {
+        inscritos.add(aluno)
+    }
+    
+    fun desmatricular(aluno: Aluno) {
+        inscritos.remove(aluno)
+    }
+ 
+ 	fun imprimirInscritos() {
+        println("Inscritos na formação $nome:")
+        inscritos.forEachIndexed { index, aluno ->
+            println("${index + 1}. Nome: ${aluno.nome}, Email: ${aluno.email}, Data de Nascimento: ${aluno.dataNascimento}")
+        }
+	}
+    
+    fun casdastrarFormacao(formacao: Formacao){
+        formacoes.add(formacao)
+    }
+    
+    fun removerFormacao(formacao: Formacao){
+        formacoes.remove(formacao)
+    }
+    
+    fun imprimirConteudos(){
+        println("Conteúdos disponíveis na formação $nome:")
+        conteudos.forEachIndexed { index, conteudos ->
+            println("${index + 1}. Nome: ${conteudos.nome}, Duração: ${conteudos.duracao}, Descrição: ${conteudos.descricao}, Nível de Dificuldade: ${conteudos.nivelDificuldade}")
+        }
+        
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val aluno1 = Aluno("Alice", "alice@example.com", "12/03/1990")
+    val aluno2 = Aluno("Bob", "bob@example.com", "30/10/1991")
+
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 90, "Conteúdo introdutório sobre programação", Nivel.BASICO, "Programação")
+    val conteudo2 = ConteudoEducacional("Algoritmos e Estruturas de Dados", 120, "Estudo de algoritmos e estruturas de dados", Nivel.INTERMEDIARIO, "Programação")
+    val conteudo3 = ConteudoEducacional("Algoritmo", 360, "Estudo avançado de algoritmos", Nivel.DIFICIL, "Programação")
+
+    val formacao1 = Formacao("Desenvolvimento de Software com Kotlin", listOf(conteudo1, conteudo2))
+    
+    val formacao2 = Formacao("Algoritmo Avançado", listOf(conteudo3))
+	
+    
+    formacao1.matricular(aluno1)
+    formacao1.matricular(aluno2)
+   
+    formacao1.imprimirInscritos()
+    println()
+    
+    formacao1.desmatricular(aluno2)
+    
+    val aluno3 = Aluno("Lucas", "lucas@example.com", "20/5/1992")
+    formacao1.matricular(aluno3)
+    formacao2.matricular(aluno2)
+    
+    formacao1.imprimirInscritos()
+    println()
+    formacao2.imprimirInscritos()
+    println()
+    formacao1.imprimirConteudos()
+    
+    
+    
 }
